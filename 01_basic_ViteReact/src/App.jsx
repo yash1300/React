3ebd6caf0a2355087913b04bc1939f904032638c
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2023-12-29 00:03:19 +0530

    Add files via upload

---
+++ 01_basic_ViteReact/src/App.jsx
@@ -0,0 +1,11 @@
+
+
+function App() {
+  
+
+  return (
+   <h1>Yash ka app or website using Vite react | Yash Vishwakarma</h1>
+  )
+}
+
+export default App
---
+++ 01_basic_ViteReact/src/yash.jsx
@@ -0,0 +1,14 @@
+// to make any function and to export it 
+// it is case sensetive first word should be capital
+// and then exprt it from the function file 
+// and import it in the main file
+
+function Yash(){
+    return (
+        <h3>DON DON DON!!!</h3>
+    )
+}
+
+// this is called as components and not function
+
+export default Yash
