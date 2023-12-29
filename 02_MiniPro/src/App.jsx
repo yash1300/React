ccbecb27a1306370e6792499e564be0407ab4706
Yash Vishwakarma <84791923+yash1300@users.noreply.github.com>
2023-12-29 15:09:21 +0530

    Add files via upload

---
+++ 02_MiniPro/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  let [counter, setCounter] = useState(0)
+  // let Counter = 15
+  let addValue = () => {
+
+    setCounter(counter + 1)
+    console.log('Value added',counter);
+  }
+
+  let removeValue  = () => {
+    if (counter > 0) {
+      setCounter(counter - 1);
+    } else {
+      console.log('Counter is already at zero. Cannot decrease further.');
+    }
+  }
+
+  return (
+    <>
+      <h2>Hello everyone YASH here!!</h2>
+      <p>Tap on increase to increase the number and on decrease to decrease the number</p>
+
+      <br />
+
+        <h4>Counter value:{counter}</h4>
+
+      <br />
+      <div>
+        <button 
+        onClick={addValue}> 
+          increase
+          </button>
+        <br /><br />
+        <button
+        onClick={removeValue}>
+        decrease
+        </button>
+      </div>
+    </>
+  )
+}
+
+export default App
